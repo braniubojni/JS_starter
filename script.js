0ea67be0b1a3fbf3ccce7a93fb9d1eef849e0ac8
braniubojni.home <braniubojni@mail.ru>
2020-06-29 00:16:50 +0400

    second task

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+let money = prompt("Ваш бюджет на месяц?", ""),
+    time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
 
-alert("Hello World double 2");
+appData = {
+    budjet : money,
+    timeData : time,
+    expenses : {},
+    optionalExpenses : {},
+    income : [],
+    savings : false
+};
+
+
+appData.expenses = prompt("Введите обязательную статью расходов в этом месяце", "");
+appData.expenses = prompt("Во сколько обойдется?", "");
+
+alert(money / 30);
