5be5b6ce45eee701493d35e2463d2281a56a8ce5
braniubojni.home <braniubojni@mail.ru>
2020-07-01 11:52:28 +0400

    Done with second task

--- script.js
+++ script.js
@@ -11,8 +11,19 @@ appData = {
     savings : false
 };
 
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        b = prompt("Во сколько обойдется?", "");
+    if (typeof(a) ==='string' && typeof(a) != null && typeof(b) != null && a != '' && b != '' && a.length < 50)  {
+        console.log('Done')
+        appData.expenses[a] = b;
+    } else {
+        console.log('You write something wrong')
+        --i;
+        continue;
+    }
+    
+}
+appData.moneyPerDay = appData.budjet / 30
 
-appData.expenses = prompt("Введите обязательную статью расходов в этом месяце", "");
-appData.expenses = prompt("Во сколько обойдется?", "");
-
-alert(money / 30);
+alert(`Ежедневный бюджет: ${appData.moneyPerDay}`);
